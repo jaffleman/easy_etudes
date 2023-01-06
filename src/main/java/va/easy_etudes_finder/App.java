0e5de8b0383e2abded1fb802a46b24eb0d1f5880
jaffleman <jaffleman@gmail.com>
2023-01-06 02:09:08 +0100

    lecture fichier docx ok!

--- src/main/java/va/easy_etudes_finder/App.java
+++ src/main/java/va/easy_etudes_finder/App.java
@@ -1,13 +1,27 @@
 package va.easy_etudes_finder;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import org.apache.poi.xwpf.extractor.*;
+import org.apache.poi.extractor.*;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        File f=  new File("/home/Jaffleman/Documents/COVID-19 - Attestation-sur-l-honneur.docx");
+        FileInputStream iss = null;
+                     try{
+        POITextExtractor textExtractor = ExtractorFactory.createExtractor(f);
+         XWPFWordExtractor wordExtractor = (XWPFWordExtractor) textExtractor;
+                    String contenu = wordExtractor.getText();
+                    System.out.println(contenu);
+        }
+        catch (Exception e) {
+                          e.printStackTrace();
+                        }
+        finally {
+            if (iss != null) iss.close();
+        }
+
     }
+
 }
